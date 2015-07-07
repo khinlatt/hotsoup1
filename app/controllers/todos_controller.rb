85c557366107161aa2494fe248452e6c9674d3df
khinlatt <k.maunglatt@gmail.com>
2015-07-06 22:14:59 -0400

    start todos

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,30 @@
+class TodosController < ApplicationController
+
+  def index
+    # you will see a collectionof todos
+  end
+
+  def create
+    # post : you create a new todo
+  end
+
+  def new
+    # get : it is where you see the page to create
+  end
+
+  def edit
+    # 
+  end
+
+  def show
+    # to show a single todo
+  end
+
+  def update
+  end
+
+  def destory
+    # to delete a single todo
+  end
+
+end
